28efa622656e2b64f3ad9b08ed2b4b7c0ffa3681
Roland Seidel <register@interpolar.ch>
2021-07-30 23:51:18 +0200

    .

--- argo/bootstrap.sh
+++ argo/bootstrap.sh
@@ -7,6 +7,8 @@ VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/
 kubectl delete -f https://raw.githubusercontent.com/argoproj/argo-cd/$VERSION/manifests/install.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/$VERSION/manifests/install.yaml
 
+kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
+
 echo "login argo-cd with user 'admin' and pw:"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
@@ -17,8 +19,6 @@ kubectl -n argocd patch deployment/argocd-dex-server -p "$(cat templates/patch-a
 kubectl apply -n argocd -f templates/argocd-cm.yaml
 
 kubectl apply -n argocd -f templates/argocd-server-ingress-route.yaml
-
-kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
 
 kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
 
