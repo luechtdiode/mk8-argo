eba6a384d8484f904c5a133f8959a1449a961975
luechtdiode <roland.seidel@interpolar.ch>
2021-12-27 17:28:34 +0100

    add namespace context to regen-secrets.sh

--- regen-secrets.sh
+++ regen-secrets.sh
@@ -2,7 +2,8 @@
 for file in $(tar -ztvf secrets.tar.gz  | awk -F' ' '{ if($NF != "") print $NF }' | xargs ); do
   if [ -e "$file" ]; then
     newname=$(echo "$file" | sed 's/-secret/-sealedsecret/')
-    kubeseal <$file -o yaml >$newname
+    namespace=$(echo $newname | cut -d/ -f2 )
+    kubeseal <$file -o yaml >$newname -n $namespace
   fi
 done
 
