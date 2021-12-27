22abf1016e421e34052a20dcbf1265f1aaecaf3f
luechtdiode <roland.seidel@interpolar.ch>
2021-12-27 21:00:09 +0100

    add kill connections during db-restore

--- backuprestore/backup.sh
+++ backuprestore/backup.sh
@@ -221,7 +221,8 @@ function dbrestore()
   echo "restoring backup from $DUMPFILE to db $DB_NAME, user $PG_USER in $NAMESPACE ..."
   postgrespod=$(kubectl -n $NAMESPACE get pod -l component=postgres -o jsonpath='{.items[*].metadata.name}')
   kubectl -n $NAMESPACE exec $postgrespod -- bash \
-    -c "dropdb -U $PG_USER --if-exists $DB_NAME && createdb -U $PG_USER -T template0 $DB_NAME"
+    -c "echo \"select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where pg_stat_activity.datname = '$DB_NAME' and pid <> pg_backend_pid();\" | psql -U kutuapp \
+        && dropdb -U $PG_USER --if-exists $DB_NAME && createdb -U $PG_USER -T template0 $DB_NAME"
   cat $DUMPFILE | kubectl -n $NAMESPACE exec -i $postgrespod -- pg_restore -U $PG_USER --no-password --section=pre-data --section=data --section=post-data --clean --dbname $DB_NAME
   echo "restore finished"
 }
