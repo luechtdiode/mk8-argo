e968f71d3939c0073bcd35cc666e1458746e5dd8
Roland Seidel <register@interpolar.ch>
2021-12-28 17:30:02 +0100

    fix dbrestore in backup.sh

--- backuprestore/backup.sh
+++ backuprestore/backup.sh
@@ -238,7 +238,7 @@ function dbrestore()
   echo "restoring backup from $DUMPFILE to db $DB_NAME, user $PG_USER in $NAMESPACE ..."
   postgrespod=$(findPostgresPod $NAMESPACE)
   kubectl -n $NAMESPACE exec $postgrespod -- bash \
-    -c "echo \"select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where pg_stat_activity.datname = '$DB_NAME' and pid <> pg_backend_pid();\" | psql -U kutuapp \
+    -c "echo \"select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where pg_stat_activity.datname = '$DB_NAME' and pid <> pg_backend_pid();\" | psql -U $PG_USER \
         && dropdb -U $PG_USER --if-exists $DB_NAME && createdb -U $PG_USER -T template0 $DB_NAME"
   cat $DUMPFILE | kubectl -n $NAMESPACE exec -i $postgrespod -- pg_restore -U $PG_USER --no-password --section=pre-data --section=data --section=post-data --clean --dbname $DB_NAME
   echo "restore finished"
