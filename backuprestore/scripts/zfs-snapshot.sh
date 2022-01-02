f3709f5d9d85f320f65f3561fa2177f782b6d510
Roland Seidel <register@interpolar.ch>
2022-01-02 12:00:24 +0100

    Update zfs-snapshot.sh

--- backuprestore/scripts/zfs-snapshot.sh
+++ backuprestore/scripts/zfs-snapshot.sh
@@ -3,7 +3,7 @@
 namespace=$1
 pvc=$2
 
-sed 's/pvcname/$pvc/g' zfs-snapshot.yaml | apply -n $namespace -f -
+sed 's/pvcname/$pvc/g' zfs-snapshot.yaml | kubectl -n $namespace apply -f -
 sed 's/pvcname/$pvc/g' zfs-snapshot.yaml | kubectl -n $namespace wait --for=condition=readytouse=true VolumeSnapshot apply -f -
 
 kubectl -n $namespace get volumesnapshot.snapshot
