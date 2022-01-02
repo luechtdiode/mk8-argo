596ee615a07332cccc0f2b23dbc2d2bd9b1e645f
Roland Seidel <register@interpolar.ch>
2022-01-02 12:37:11 +0100

    Update zfs-snapshot.sh

--- backuprestore/scripts/zfs-snapshot.sh
+++ backuprestore/scripts/zfs-snapshot.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-namespace=$1
-pvc=$2
+# https://docs.oracle.com/cd/E23824_01/html/821-1448/recover-1.html#scrolltoc
 
-sed 's/pvcname/$pvc/g' zfs-snapshot.yaml | kubectl -n $namespace apply -f -
-sed 's/pvcname/$pvc/g' zfs-snapshot.yaml | kubectl -n $namespace wait --for=condition=readytouse=true VolumeSnapshot apply -f -
+ZFS_POOL=zfs-pvpool
 
-kubectl -n $namespace get volumesnapshot.snapshot
+function zfs_backup() {
+  namespace=$1
+  TARGET=$2
+  pvc=$3
+
+  sed 's/pvcname/$pvc/g' zfs-snapshot.yaml | kubectl -n $namespace apply -f -
+  sed 's/pvcname/$pvc/g' zfs-snapshot.yaml | kubectl -n $namespace wait --for=condition=readytouse=true VolumeSnapshot apply -f -
+
+  kubectl -n $namespace get volumesnapshot.snapshot
+  snapshot=$(kubectl -n $namespace  get volumesnapshot.snapshot -o jsonpath='{.items[*].status.boundVolumeSnapshotContentName}')
+  zfs send -Rv "${ZFS_POOL}/${pvc}@${snapshot}" | gzip > $TARGET/$snapshot
+}
+
+function zfs_restore() {
+  namespace=$1
+  pvc=kubectl -n $namespace get volumesnapshot.snapshot -o jsonpath='{.items[*].spec.source.persistentVolumeClaimName}'
+  snapshots=$(kubectl -n $namespace get volumesnapshot.snapshot -o jsonpath='{.items[*].status.boundVolumeSnapshotContentName}')
+  
+  for snapshot in $snapshots | xargs do;
+    gzcat $TARGET/$snapshot | zfs receive -Fv "${ZFS_POOL}/${pvc}@${snapshot}"
+  done
+}
