6f9d4559c0fc02fe05dc6d1724f0d486514080fd
Roland Seidel <register@interpolar.ch>
2022-04-01 21:11:52 +0200

    bootstrap.sh with sealed-secret & traefik

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,30 @@
 helm -n argocd uninstall argocd
 kubectl delete namespace argocd
 
+helm -n sealed-secrets uninstall sealed-secrets
+kubectl delete namespace sealed-secrets
+
+helm -n traefik uninstall traefik
+kubectl delete namespace traefik
+
+cd sealed-secrets
+helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+helm repo update
+helm dependencies update
+helm install -n sealed-secrets . -f values.yaml
+cd ..
+
+cd traefik
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+helm dependencies update
+helm template traefik traefik/traefik -f values.yaml --debug
+kubectl delete namespace traefik
+kubectl create namespace traefik
+helm install -n traefik traefik/traefik . -f values.yaml
+helm upgrade -n traefik traefik/traefik . -f values.yaml
+cd ..
+
 cd argocd
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm repo update
