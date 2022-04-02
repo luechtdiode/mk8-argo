f4f7b37a968b5728cf2a7c5f0a2b134864df3730
Roland Seidel <register@interpolar.ch>
2022-04-02 12:22:41 +0200

    mbp wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
 
-helm -n argocd uninstall argocd
-kubectl delete namespace argocd
+# setup micok8s from ground up
+sudo snap remove microk8s
+rm -rf $(pwd)/.kube
+mkdir $(pwd)/.kube
+sudo chown -f -R $USER $(pwd)/.kube
 
-helm -n sealed-secrets uninstall sealed-secrets
-kubectl delete namespace sealed-secrets
+snap info microk8s
+sudo snap install microk8s --classic --channel=latest/stable
+sudo usermod -a -G microk8s $USER
+su - $USER
+microk8s status --wait-ready
+microk8s enable helm3 host-access ingress metrics-server dns openebs rbac storage
 
-helm -n traefik uninstall traefik
-kubectl delete namespace traefik
+alias kubectl='microk8s kubectl'
+alias helm='microk8s helm3'
+
+microk8s config > $(pwd)/.kube/admin.config
+export KUBECONFIG=$(pwd)/.kube/admin.config
+
+# prepare pre-requisites needed to apply gitops via argocd
 
 cd sealed-secrets
+wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.16.0/kubeseal-linux-amd64 -O kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm repo update
 helm dependencies update
 helm install -n sealed-secrets . -f values.yaml
+cd ..
+
+curl -L https://github.com/storj/storj/releases/latest/download/uplink_linux_amd64.zip -o uplink_linux_amd64.zip
+unzip -o uplink_linux_amd64.zip && rm uplink_linux_amd64.zip
+sudo install -m 755 uplink /usr/local/bin/uplink
+uplink setup # 2 eu1.storj.io, access-name, api-key, passphrase, passphrase, n
+
+
+cd backuprestore
+./main.sh cloudsync down
+./main.sh secretrestore
 cd ..
 
 cd traefik
