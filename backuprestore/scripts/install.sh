9dea7d92553c8868c28769e53b576df4f626a274
Roland Seidel <register@interpolar.ch>
2022-06-06 18:38:08 +0200

    remove as directory

--- backuprestore/scripts/install.sh
+++ backuprestore/scripts/install.sh
@@ -2,11 +2,16 @@
 
 function install()
 {
-  croncmd="kubectl=$(which kubectl) && $(pwd)/main.sh short"
-  sudo crontab -u root -l | grep -v 'backuprestore/' > crontabcleaned.txt
+  croncmd="$(pwd)/main.sh short"
+  pathline="PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
+  sudo crontab -u root -l > crontabcleaned.txt
+  cat crontabcleaned.txt | grep -v 'backuprestore/'
+  cat crontabcleaned.txt | grep -v 'PATH='
+
   cp crontabcleaned.txt crontabupdated.txt
   #    22 2 * * * => every day at 02:22
-  echo "22 2 * * * $croncmd >> $(pwd)/backup.log 2>&1" >> crontabupdated.txt
+  echo "$pathline >> $(pwd)/backup.log 2>&1" >> crontabupdated.txt
+  echo "00,10,20,30,40,50 * * * * $croncmd >> $(pwd)/backup.log 2>&1" >> crontabupdated.txt
   sudo crontab -u root crontabupdated.txt
 
   sudo rm /etc/sudoers.d/mk8backuprestore
