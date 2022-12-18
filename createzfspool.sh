ea257025a2ba02ebd2148565a7918e6fadb65235
Roland Seidel <register@interpolar.ch>
2022-12-19 00:22:13 +0100

    Update createzfspool.sh

--- createzfspool.sh
+++ createzfspool.sh
@@ -3,12 +3,21 @@
 sudo apt-get install zfsutils-linux
 wait
 
-if ! [[ "$(zfs list | grep zfspv-pool | grep -v legacy)" == */var/snap/microk8s/common/var/openebs/local ]]
-then
-  if ! [[ "$(zpool status zfspv-pool | grep state:)" == *ONLINE]]
+function zfsDetachPool() {
+  if ! [[ "$(zfs list | grep zfspv-pool | grep -v legacy)" == */var/snap/microk8s/common/var/openebs/local ]]
   then
-    for i in {1..3}; do sudo truncate -s 100G /zfsdisks/$i.img; done
-    sudo zpool create zfspv-pool raidz1 /zfsdisks/1.img /zfsdisks/2.img /zfsdisks/3.img
-  fi  
-  sudo zfs set mountpoint=/var/snap/microk8s/common/var/openebs/local zfspv-pool
-fi
+    sudo zfs set mountpoint=none zfspv-pool && zfs unmount /var/snap/microk8s/common/var/openebs/local
+  fi
+}
+
+function zfsInitPool() {
+  if ! [[ "$(zfs list | grep zfspv-pool | grep -v legacy)" == */var/snap/microk8s/common/var/openebs/local ]]
+  then
+    if ! [[ "$(zpool status zfspv-pool | grep state:)" == *ONLINE]]
+    then
+      for i in {1..3}; do sudo truncate -s 100G /zfsdisks/$i.img; done
+      sudo zpool create zfspv-pool raidz1 /zfsdisks/1.img /zfsdisks/2.img /zfsdisks/3.img
+    fi  
+    sudo zfs set mountpoint=/var/snap/microk8s/common/var/openebs/local zfspv-pool
+  fi
+}
