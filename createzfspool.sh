32e9e6fa8aa73202ee3e1b6480855d48d979702a
Roland Seidel <register@interpolar.ch>
2022-12-19 00:28:09 +0100

    Update createzfspool.sh

--- createzfspool.sh
+++ createzfspool.sh
@@ -13,7 +13,7 @@ function zfsDetachPool() {
 function zfsInitPool() {
   if ! [[ "$(zfs list | grep zfspv-pool | grep -v legacy)" == */var/snap/microk8s/common/var/openebs/local ]]
   then
-    if ! [[ "$(zpool status zfspv-pool | grep state:)" == *ONLINE]]
+    if ! [[ "$(zpool status zfspv-pool | grep state:)" == *ONLINE ]]
     then
       for i in {1..3}; do sudo truncate -s 100G /zfsdisks/$i.img; done
       sudo zpool create zfspv-pool raidz1 /zfsdisks/1.img /zfsdisks/2.img /zfsdisks/3.img
