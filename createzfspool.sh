a7ea74926bec01140bdaeab21b20afc1884ee424
Roland Seidel <register@interpolar.ch>
2022-12-26 15:21:19 +0100

    reduce initial zfs-pool size

--- createzfspool.sh
+++ createzfspool.sh
@@ -15,8 +15,10 @@ function zfsInitPool() {
   then
     if ! [[ "$(zpool status zfspv-pool | grep state:)" == *ONLINE ]]
     then
-      for i in {1..3}; do sudo truncate -s 100G /zfsdisks/$i.img; done
-      sudo zpool create zfspv-pool raidz1 /zfsdisks/1.img /zfsdisks/2.img /zfsdisks/3.img
+      # for i in {1..3}; do sudo truncate -s 40G /zfsdisks/$i.img; done
+      # sudo zpool create zfspv-pool raidz1 /zfsdisks/1.img /zfsdisks/2.img /zfsdisks/3.img
+      sudo truncate -s 40G /zfsdisks/zfspv-pool.img
+      sudo zpool create zfspv-pool /zfsdisks/zfspv-pool.img
     fi  
     sudo zfs set mountpoint=/var/snap/microk8s/common/var/openebs/local zfspv-pool
   fi
