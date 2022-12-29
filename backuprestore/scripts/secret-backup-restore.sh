e505e1a85fbb176d24752f39d174edf5049d2b6a
Roland Seidel <register@interpolar.ch>
2022-12-30 00:04:45 +0100

    enhance secret-backup-restore

--- backuprestore/scripts/secret-backup-restore.sh
+++ backuprestore/scripts/secret-backup-restore.sh
@@ -1,18 +1,63 @@
 
 function secretbackup() {
+  maxageSeconds=$(echo 365*24*3600 | bc)
   # find from mk8-argo project-root (backuprestore/..)
-  for pair in $(kubectl get secret --all-namespaces -l sealedsecrets.bitnami.com/sealed-secrets-key -o jsonpath='{range .items[*]}{ ..namespace }{","}{..name}{"\n"}{end}'); do
-    IFS=, read -r namespace secret <<< "$pair"
-    echo "getting sealed-secret private-key $secret from $namespace"
-    mkdir -p ../$namespace/templates
-    kubectl get secret $secret -n $namespace -o yaml > ../$namespace/templates/backup-$secret-secret-private.yaml
+  
+  echo "deleting previous collected secret-private files in local filesystem:"
+  find ../* -type f -name "*-secret-private.yaml"
+  find ../* -type f -name "*-secret-private.yaml" | xargs rm -f
+
+  for pair in $(kubectl get secret --all-namespaces -l sealedsecrets.bitnami.com/sealed-secrets-key=active -o jsonpath='{range .items[*]}{ ..namespace }{","}{..name}{","}{..creationTimestamp}{"\n"}{end}'); do
+    IFS=, read -r namespace secret creationTimestamp <<< "$pair"
+    age="$(($(date +%s) - $(date -d $creationTimestamp +%s)))"
+    #if [ $maxageSeconds -ge $age ]
+    #then
+      echo "getting sealed-secret private-key $secret from $namespace"
+      mkdir -p ../$namespace/templates
+      kubectl get secret $secret -n $namespace -o yaml > ../$namespace/templates/backup-$secret-secret-private.yaml
+    #else
+    #  echo "NOT getting sealed-secret private-key $secret from $namespace."
+    #  echo " MaxAge in seconds $maxageSeconds."
+    #  echo " Age in seconds    $age."
+    #fi
   done
+
+  # find_custom_secrets_in_cluster
+  find_sealed_secrets_in_cluster
 
   find ../* -type f -name "*-secret.yaml" -o -name "*-secret-*.yaml" | xargs tar -czf secrets.tar.gz
   echo "secrets collected and saved to secrets.tar.gz"
 }
 
+function find_sealed_secrets_in_cluster() {
+  echo "deleting previous collected backup-*-secret files in local filesystem:"
+  find ../* -type f -name "backup-*-secret.yaml"
+  find ../* -type f -name "backup-*-secret.yaml" | xargs rm -f
+  for pair in $(kubectl get sealedsecret --all-namespaces -o go-template='{{range .items }}{{printf "%s,%s\n" .metadata.namespace .metadata.name }}{{end}}')
+  do
+    IFS=, read -r namespace secret <<< "$pair"
+    echo "getting secret of sealedsecret $secret from $namespace"
+    mkdir -p ../$namespace/templates
+    kubectl get secret $secret -n $namespace -o yaml > ../$namespace/templates/backup-$secret.yaml
+  done
+}
+
+# unused ...
+function find_custom_secrets_in_cluster() {
+  echo "deleting previous collected backup-*-secret files in local filesystem:"
+  find ../* -type f -name "backup-*-secret.yaml"
+  find ../* -type f -name "backup-*-secret.yaml" | xargs rm -f
+  for pair in $(kubectl get secret --field-selector type=Opaque --all-namespaces -o go-template='{{range .items }}{{if .metadata.labels }}{{else if eq .metadata.name "memberlist" }}{{else}}{{printf "%s,%s\n" .metadata.namespace .metadata.name }}{{end}}{{end}}')
+  do
+    IFS=, read -r namespace secret <<< "$pair"
+    echo "getting custom-secret $secret from $namespace"
+    mkdir -p ../$namespace/templates
+    kubectl get secret $secret -n $namespace -o yaml > ../$namespace/templates/backup-$secret.yaml
+  done
+}
+
 function restoreSecret() {
+  [[ $1 == */backup-*-secret.yaml ]] && applySecret $1
   newname=$(echo "../$1" | sed 's/-secret/-sealedsecret/')
   namespace=$(echo $newname | cut -d/ -f2 )
 
