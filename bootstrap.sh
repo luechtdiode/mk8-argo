8747e880651250195f36045320ec115949997838
Roland Seidel <register@interpolar.ch>
2023-10-12 15:21:10 +0200

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -224,11 +224,11 @@ function toggleHarborMirror() {
     waitForDeployment harbor harbor-core
     waitForDeployment harbor harbor-portal
     waitForDeployment harbor harbor-jobservice
-  else if [[ ! askn "Should harbor-mirror be used fom now on?" ]]
+  elif [[ ! askn "Should harbor-mirror be used fom now on?" ]]
     cp /var/snap/microk8s/current/args/certs.d/docker.io/hosts.toml ./original-dockerio-host.toml
     nano harbor-mirror-host.toml
     cp harbor-mirror-host.toml /var/snap/microk8s/current/args/certs.d/docker.io/hosts.toml
-    
+
     sudo microk8s stop
     sudo microk8s start
     waitForDeployment traefik traefik
