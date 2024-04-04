a16d5e994890c5be47a6fad18a9d7df8361c8bf3
Roland Seidel <register@interpolar.ch>
2024-04-04 22:48:31 +0200

    prepare preview-params

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ then
   git pull
   cd ..
 else
-  git clone -b mk8-128 --single-branch https://github.com/luechtdiode/mk8-argo.git
+  git clone -b mk8-128-preview --single-branch https://github.com/luechtdiode/mk8-argo.git
 fi
 
 sudo microk8s stop
